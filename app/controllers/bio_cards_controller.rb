f02cf3d237db6a4ee36d450a5bdc0c5c410c72cb
Kamrin <kamrinkennedy@gmail.com>
2020-09-26 16:05:17 -0500

    biocard crud

--- app/controllers/bio_cards_controller.rb
+++ app/controllers/bio_cards_controller.rb
@@ -1,6 +1,13 @@
 class BioCardsController < ApplicationController
 
     def create
+        bio_card = BioCard.new(bio_card_params)
+        if bio_card.save
+            render json: bio_card
+        else
+            messages = biocard.errors.full_messages.join(', ')
+            render json: {error: messages}
+        end
     end
 
     def edit
@@ -21,7 +28,7 @@ class BioCardsController < ApplicationController
 
     private
     def bio_card_params
-        params.require(:bio_card).permit(:age, :weight, :height, :eye_color, :hair_color)
+        params.require(:bio_card).permit(:age, :weight, :height, :eye_color, :hair_color, :actor_id)
     end
 
 end
